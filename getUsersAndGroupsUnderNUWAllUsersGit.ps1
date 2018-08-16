c6029a004184ea4e22ee5a77946643c7df3b812a
kpilsbury <kpilsbury@users.noreply.github.com>
2018-08-17 09:12:18 +1000

    added code

---
+++ getUsersAndGroupsUnderNUWAllUsersGit.ps1
@@ -0,0 +1,27 @@
+﻿$filepath = "<File Path to dump as TXT>"
+$ADGroup = "<Name of Group to browse Recursive tree memberships>"
+
+function outputADGroupMembership ([String]$groupname)
+{
+    Out-File -FilePath $filepath -InputObject $groupname -Width 80
+    $Accounts = get-adgroupmember $groupname | select name, objectclass | Sort objectclass,name
+    Out-File -FilePath $filepath -InputObject $Accounts -Append -Width 80
+    $groups = get-adgroupmember $groupname | where objectclass -eq group | Sort objectclass,name
+    foreach ($group in $groups){
+        outputADGroupMembershipAppend ($group)
+    }
+}
+
+function outputADGroupMembershipAppend ([String]$groupname)
+{
+    $gn = get-adgroup -Filter {DistinguishedName -eq $groupname} | select name
+    Out-File -FilePath $filepath -InputObject $gn.name -Append -Width 80
+    $Accounts  = get-adgroupmember $groupname | select name, objectclass | Sort objectclass,name
+    Out-File -FilePath $filepath -InputObject $Accounts -Append -Width 80
+    $groups = get-adgroupmember $groupname | where objectclass -eq group | Sort objectclass,name
+    foreach ($group in $groups){
+        outputADGroupMembershipAppend ($group)
+    }
+}
+
+outputADGroupMembership($ADGroup)
